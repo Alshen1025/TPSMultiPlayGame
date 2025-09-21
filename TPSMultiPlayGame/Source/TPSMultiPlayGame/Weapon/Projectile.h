8a11a7206c5a7c7d9c98a8cbca7c9ab9101e72e1
Alshen1025 <seonghyeongo25@gmail.com>
2025-09-21 18:25:08 +0900

    Missing classes file

---
+++ TPSMultiPlayGame/Source/TPSMultiPlayGame/Weapon/Projectile.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Projectile.generated.h"
+
+UCLASS()
+class TPSMULTIPLAYGAME_API AProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AProjectile();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+private:
+
+	UPROPERTY(EditAnywhere)
+	class UBoxComponent* CollisionBox;
+
+};
