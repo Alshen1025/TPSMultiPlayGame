bdaba1db10609421083c39545f2c9ac4b6299f9a
Alshen1025 <seonghyeongo25@gmail.com>
2025-10-08 05:13:02 +0900

    Implement Projectile Weapon, HitScanWeapon and Shotgun ServerSideRewind

--- TPSMultiPlayGame/Source/TPSMultiPlayGame/TPSComponents/LagCompensationComponent.cpp
+++ TPSMultiPlayGame/Source/TPSMultiPlayGame/TPSComponents/LagCompensationComponent.cpp
@@ -5,12 +5,15 @@
 #include "TPSMultiPlayGame/Public/Character/TPSCharacter.h"
 #include "Components/BoxComponent.h"
 #include "DrawDebugHelpers.h"
+#include "Kismet/GameplayStatics.h"
+#include "TPSMultiPlayGame/Weapon/Weapon.h"
+#include "TPSMultiPlayGame/TPSMultiPlayGame.h"
 
 ULagCompensationComponent::ULagCompensationComponent()
 {
 	PrimaryComponentTick.bCanEverTick = true;
-
 }
+
 void ULagCompensationComponent::BeginPlay()
 {
 	Super::BeginPlay();
@@ -18,6 +21,12 @@ void ULagCompensationComponent::BeginPlay()
 void ULagCompensationComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	SaveFramePackage();
+}
+void ULagCompensationComponent::SaveFramePackage()
+{
+	//Frame의 저장은 서버에서만 해야함
+	if (Character == nullptr || !Character->HasAuthority()) return;
 	if (FrameHistroy.Num() <= 1)
 	{
 		FFramePackage ThisFrame;
@@ -38,11 +47,9 @@ void ULagCompensationComponent::TickComponent(float DeltaTime, ELevelTick TickTy
 		FrameHistroy.AddHead(ThisFrame);
 
 		//디버그 용 박스 그리기
-		ShowFramePackage(ThisFrame, FColor::Red);
+		//ShowFramePackage(ThisFrame, FColor::Red);
 	}
 }
-
-
 void ULagCompensationComponent::SaveFramePackage(FFramePackage& Package)
 {
 	Character = Character == nullptr ? Cast<ATPSCharacter>(GetOwner()) : Character;
@@ -50,6 +57,7 @@ void ULagCompensationComponent::SaveFramePackage(FFramePackage& Package)
 	{
 		//시간 저장
 		Package.Time = GetWorld()->GetTimeSeconds();
+		Package.Character = Character;
 
 		//그 시간의 HitBoxes들의 정보
 		for (auto& BoxPair : Character->HitBoxes)
@@ -62,7 +70,6 @@ void ULagCompensationComponent::SaveFramePackage(FFramePackage& Package)
 		}
 	}
 }
-
 FFramePackage ULagCompensationComponent::InterpBetweenFrams(const FFramePackage& OlderFrame, const FFramePackage& YoungerFrame, float HitTime)
 {
 	//두 프레임 사이의 간격
@@ -102,7 +109,232 @@ FFramePackage ULagCompensationComponent::InterpBetweenFrams(const FFramePackage&
 
 	return InterpFramePackage;
 }
+FServerSideRewindResult ULagCompensationComponent::ConfirmHit(const FFramePackage& Package, ATPSCharacter* HitCharacter, const FVector_NetQuantize& TraceStart, const FVector_NetQuantize& HitLocation)
+{
+	if (HitCharacter == nullptr) return FServerSideRewindResult();
+	//현재 프레임의 패키지 정보를 저장
+	FFramePackage CurrentFrame;
+	CacheBoxPositions(HitCharacter, CurrentFrame);
+	//피격 당시 시간대의 위치로 HitBox이동
+	MoveBoxes(HitCharacter, Package);
+	EnableCharacterMeshCollision(HitCharacter, ECollisionEnabled::NoCollision);
 
+	//머리(HeadShot)에 대한 충돌 먼저 활성화
+	UBoxComponent* HeadBox = HitCharacter->HitBoxes[FName("head")];
+	HeadBox->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	HeadBox->SetCollisionResponseToChannel(ECC_HitBox, ECollisionResponse::ECR_Block);
+
+	//피격 당시 시간대로 옮긴 Box에 대해서 라인 트래이스 실행
+	FHitResult ConfirmHitResult;
+	const FVector TraceEnd = TraceStart + (HitLocation - TraceStart) * 1.25f;
+	UWorld* World = GetWorld();
+	if (World)
+	{
+		World->LineTraceSingleByChannel(
+			ConfirmHitResult,
+			TraceStart,
+			TraceEnd,
+			ECC_HitBox
+		);
+		if (ConfirmHitResult.bBlockingHit) //머리를 맞았을 경우를 가장 먼저 판정
+		{
+			//HitBox들 제자리로
+			ResetHitBoxes(HitCharacter, CurrentFrame);
+			EnableCharacterMeshCollision(HitCharacter, ECollisionEnabled::QueryAndPhysics);
+			//결과 (피격 true, 헤드샷 true 리턴)
+			return FServerSideRewindResult{ true, true };
+		}
+		else //맞은게 머리가 아닌 경우에 나머지 박스에 대한 충돌 활성화 후 라인트레이스
+		{
+			for (auto& HitBoxPair : HitCharacter->HitBoxes)
+			{
+				if (HitBoxPair.Value != nullptr)
+				{
+					HitBoxPair.Value->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+					HitBoxPair.Value->SetCollisionResponseToChannel(ECC_HitBox, ECollisionResponse::ECR_Block);
+				}
+			}
+			World->LineTraceSingleByChannel(
+				ConfirmHitResult,
+				TraceStart,
+				TraceEnd,
+				ECC_HitBox
+			);
+			if (ConfirmHitResult.bBlockingHit)
+			{
+				//Box원래대로
+				ResetHitBoxes(HitCharacter, CurrentFrame);
+				EnableCharacterMeshCollision(HitCharacter, ECollisionEnabled::QueryAndPhysics);
+				//결과
+				return FServerSideRewindResult{ true, false };
+			}
+		}
+	}
+	//그 어디에도 피격되지 않음
+	ResetHitBoxes(HitCharacter, CurrentFrame);
+	EnableCharacterMeshCollision(HitCharacter, ECollisionEnabled::QueryAndPhysics);
+	return FServerSideRewindResult{ false, false };
+}
+FShotgunServerSideRewindResult ULagCompensationComponent::ShotgunConfirmHit(const TArray<FFramePackage>& FramePackages, const FVector_NetQuantize& TraceStart, const TArray<FVector_NetQuantize>& HitLocations)
+{
+	for (auto& Frame : FramePackages)
+	{
+		if (Frame.Character == nullptr) return FShotgunServerSideRewindResult();
+	}
+	FShotgunServerSideRewindResult ShotgunResult;
+	TArray<FFramePackage> CurrentFrames;
+	for (auto& Frame : FramePackages)
+	{
+		if (Frame.Character == nullptr) continue;
+		FFramePackage CurrentFrame;
+		CurrentFrame.Character = Frame.Character;
+		CacheBoxPositions(Frame.Character, CurrentFrame);
+		MoveBoxes(Frame.Character, Frame);
+		EnableCharacterMeshCollision(Frame.Character, ECollisionEnabled::NoCollision);
+		CurrentFrames.Add(CurrentFrame);
+	}
+	for (auto& Frame : FramePackages)
+	{
+		if (Frame.Character == nullptr) continue;
+		//헤드 판정 먼저
+		UBoxComponent* HeadBox = Frame.Character->HitBoxes[FName("head")];
+		HeadBox->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+		HeadBox->SetCollisionResponseToChannel(ECC_HitBox, ECollisionResponse::ECR_Block);
+	}
+	UWorld* World = GetWorld();
+	//헤드샷 관련 라인트레이스
+	for (auto& HitLocation : HitLocations)
+	{
+		FHitResult ConfirmHitResult;
+		const FVector TraceEnd = TraceStart + (HitLocation - TraceStart) * 1.25f;
+		if (World)
+		{
+			World->LineTraceSingleByChannel(
+				ConfirmHitResult,
+				TraceStart,
+				TraceEnd,
+				ECC_HitBox
+			);
+			ATPSCharacter* TPSCharacter = Cast<ATPSCharacter>(ConfirmHitResult.GetActor());
+			if (TPSCharacter)
+			{
+				if (ShotgunResult.HeadShots.Contains(TPSCharacter))
+				{
+					ShotgunResult.HeadShots[TPSCharacter]++;
+				}
+				else
+				{
+					ShotgunResult.HeadShots.Emplace(TPSCharacter, 1);
+				}
+			}
+		}
+	}
+	//헤드 이후에는 Body판정
+	for (auto& Frame : FramePackages)
+	{
+		if (Frame.Character == nullptr) continue;
+		for (auto& HitBoxPair : Frame.Character->HitBoxes)
+		{
+			if (HitBoxPair.Value != nullptr)
+			{
+				//Body박스 활성화
+				HitBoxPair.Value->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+				HitBoxPair.Value->SetCollisionResponseToChannel(ECC_HitBox, ECollisionResponse::ECR_Block);
+			}
+		}
+		//HeadBox는 비활성화
+		UBoxComponent* HeadBox = Frame.Character->HitBoxes[FName("head")];
+		HeadBox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	}
+
+	for (auto& HitLocation : HitLocations)
+	{
+		//Body에 대한 판정
+		FHitResult ConfirmHitResult;
+		const FVector TraceEnd = TraceStart + (HitLocation - TraceStart) * 1.25f;
+		if (World)
+		{
+			World->LineTraceSingleByChannel(
+				ConfirmHitResult,
+				TraceStart,
+				TraceEnd,
+				ECC_HitBox
+			);
+			ATPSCharacter* TPSCharacter = Cast<ATPSCharacter>(ConfirmHitResult.GetActor());
+			if (TPSCharacter)
+			{
+				if (ShotgunResult.BodyShots.Contains(TPSCharacter))
+				{
+					ShotgunResult.BodyShots[TPSCharacter]++;
+				}
+				else
+				{
+					ShotgunResult.BodyShots.Emplace(TPSCharacter, 1);
+				}
+			}
+		}
+	}
+	//모든 박스 원위치
+	for (auto& Frame : CurrentFrames)
+	{
+		if (Frame.Character == nullptr) continue;
+		ResetHitBoxes(Frame.Character, Frame);
+		EnableCharacterMeshCollision(Frame.Character, ECollisionEnabled::QueryAndPhysics);
+	}
+	return ShotgunResult;
+}
+void ULagCompensationComponent::CacheBoxPositions(ATPSCharacter* HitCharacter, FFramePackage& OutFramePackage)
+{
+	if (HitCharacter == nullptr) return;
+	//HitBox순회
+	for (auto& HitBoxPair : HitCharacter->HitBoxes)
+	{
+		if (HitBoxPair.Value != nullptr)
+		{
+			FBoxInformation BoxInfo;
+			BoxInfo.Location = HitBoxPair.Value->GetComponentLocation();
+			BoxInfo.Rotation = HitBoxPair.Value->GetComponentRotation();
+			BoxInfo.BoxExtent = HitBoxPair.Value->GetScaledBoxExtent();
+			OutFramePackage.HitBoxInfo.Add(HitBoxPair.Key, BoxInfo);
+		}
+	}
+}
+void ULagCompensationComponent::MoveBoxes(ATPSCharacter* HitCharacter, const FFramePackage& Package)
+{
+	if (HitCharacter == nullptr) return;
+	for (auto& HitBoxPair : HitCharacter->HitBoxes)
+	{
+		//Package의 값으로 현재 캐릭터의 HitBox들 이동
+		if (HitBoxPair.Value != nullptr)
+		{
+			HitBoxPair.Value->SetWorldLocation(Package.HitBoxInfo[HitBoxPair.Key].Location);
+			HitBoxPair.Value->SetWorldRotation(Package.HitBoxInfo[HitBoxPair.Key].Rotation);
+			HitBoxPair.Value->SetBoxExtent(Package.HitBoxInfo[HitBoxPair.Key].BoxExtent);
+		}
+	}
+}
+void ULagCompensationComponent::ResetHitBoxes(ATPSCharacter* HitCharacter, const FFramePackage& Package)
+{
+	if (HitCharacter == nullptr) return;
+	for (auto& HitBoxPair : HitCharacter->HitBoxes)
+	{
+		//Package의 값으로 현재 캐릭터의 HitBox들 이동
+		if (HitBoxPair.Value != nullptr)
+		{
+			HitBoxPair.Value->SetWorldLocation(Package.HitBoxInfo[HitBoxPair.Key].Location);
+			HitBoxPair.Value->SetWorldRotation(Package.HitBoxInfo[HitBoxPair.Key].Rotation);
+			HitBoxPair.Value->SetBoxExtent(Package.HitBoxInfo[HitBoxPair.Key].BoxExtent);
+			HitBoxPair.Value->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+		}
+	}
+}
+void ULagCompensationComponent::EnableCharacterMeshCollision(ATPSCharacter* HitCharacter, ECollisionEnabled::Type CollisionEnabled)
+{
+	if (HitCharacter && HitCharacter->GetMesh())
+	{
+		HitCharacter->GetMesh()->SetCollisionEnabled(CollisionEnabled);
+	}
+}
 void ULagCompensationComponent::ShowFramePackage(const FFramePackage& Package, FColor Color)
 {
 	for (auto& BoxInfo : Package.HitBoxInfo)
@@ -119,16 +351,33 @@ void ULagCompensationComponent::ShowFramePackage(const FFramePackage& Package, F
 		);
 	}
 }
-
-void ULagCompensationComponent::ServerSideRewind(ATPSCharacter* HitCharacter, const FVector_NetQuantize& TraceStart, const FVector_NetQuantize& HitLocation, float HitTime)
+FServerSideRewindResult ULagCompensationComponent::ServerSideRewind(ATPSCharacter* HitCharacter, const FVector_NetQuantize& TraceStart, const FVector_NetQuantize& HitLocation, float HitTime)
+{
+	FFramePackage FrameToCheck = GetFrameToCheck(HitCharacter, HitTime);
+	//Server-Side Rewind를 이용한 피격 판정 진행 후 결과 값 리턴
+	return ConfirmHit(FrameToCheck, HitCharacter, TraceStart, HitLocation);
+}
+FShotgunServerSideRewindResult ULagCompensationComponent::ShotgunServerSideRewind(const TArray<ATPSCharacter*>& HitCharacters, const FVector_NetQuantize& TraceStart, const TArray<FVector_NetQuantize>& HitLocations, float HitTime)
+{
+	TArray<FFramePackage> FramesToCheck;
+	//피격된 캐릭터들에게서 판정할 히트박스 정보 가져와서 리턴
+	for (ATPSCharacter* HitCharacter : HitCharacters)
+	{
+		FramesToCheck.Add(GetFrameToCheck(HitCharacter, HitTime));
+	}
+	return ShotgunConfirmHit(FramesToCheck, TraceStart, HitLocations);
+}
+FFramePackage ULagCompensationComponent::GetFrameToCheck(ATPSCharacter* HitCharacter, float HitTime)
 {
 	bool bReturn =
 		HitCharacter == nullptr ||
 		HitCharacter->GetLagCompensation() == nullptr ||
 		HitCharacter->GetLagCompensation()->FrameHistroy.GetHead() == nullptr ||
 		HitCharacter->GetLagCompensation()->FrameHistroy.GetTail() == nullptr;
+	if (bReturn) return FFramePackage();
 	//피격 되었는지 확인 해야하는 프레임의 FramePackage
 	FFramePackage FrameToCheck;
+
 	bool bShouldInterpolate = true;
 	// 피격된 캐릭터의 FrameHistroy에 접근
 	const TDoubleLinkedList<FFramePackage>& History = HitCharacter->GetLagCompensation()->FrameHistroy;
@@ -138,7 +387,7 @@ void ULagCompensationComponent::ServerSideRewind(ATPSCharacter* HitCharacter, co
 	{
 		//프레임이 기록된 시간보다 더 이전에 피격됨.
 		//피격 판정 불가
-		return;
+		return FFramePackage();
 	}
 	if (OldestHistoryTime == HitTime)
 	{
@@ -183,12 +432,192 @@ void ULagCompensationComponent::ServerSideRewind(ATPSCharacter* HitCharacter, co
 	}
 	if (bShouldInterpolate)
 	{
-		//Younger 와 Older사이를 보간
+		//Younger 와 Older사이를 보간해야함
+		FrameToCheck = InterpBetweenFrams(Older->GetValue(), Younger->GetValue(), HitTime);
+	}
+	FrameToCheck.Character = HitCharacter;
+	return FrameToCheck;
+}
+void ULagCompensationComponent::ServerScoreRequst_Implementation(ATPSCharacter* HitCharacter, const FVector_NetQuantize& TraceStart, const FVector_NetQuantize& HitLocation, float HitTime)
+{
+	//ServerSideRewind 호출해서 피격 판정 결과 얻기
+	FServerSideRewindResult Confirm = ServerSideRewind(HitCharacter, TraceStart, HitLocation, HitTime);
+
+	if (Character && HitCharacter && Confirm.bHitConfirmed)
+	{
+		UGameplayStatics::ApplyDamage(
+			HitCharacter,
+			Character->GetEquippedWeapon()->GetDamage(),
+			Character->Controller,
+			Character->GetEquippedWeapon(),
+			UDamageType::StaticClass()
+		);
+	}
+}
+void ULagCompensationComponent::ShotgunServerScoreRequest_Implementation(const TArray<ATPSCharacter*>& HitCharacters,
+	const FVector_NetQuantize& TraceStart, const TArray<FVector_NetQuantize>& HitLocations, float HitTime)
+{
+	UE_LOG(LogTemp, Warning, TEXT("--- [SERVER DEBUG] ShotgunServerScoreRequest RPC Received ---"));
+	UE_LOG(LogTemp, Warning, TEXT("[SERVER DEBUG] Number of potential targets: %d"), HitCharacters.Num());
+
+	//피격 판정
+	FShotgunServerSideRewindResult Confirm = ShotgunServerSideRewind(HitCharacters, TraceStart, HitLocations, HitTime);
+
+	// 피격 판정 결과를 상세히 로그로 출력합니다.
+	if (Confirm.HeadShots.Num() > 0)
+	{
+		for (auto& HeadShotPair : Confirm.HeadShots)
+		{
+			if (HeadShotPair.Key)
+			{
+				UE_LOG(LogTemp, Warning, TEXT("[SERVER DEBUG] Rewind Result -> HEADSHOT on %s, Pellet Count: %d"), *HeadShotPair.Key->GetName(), HeadShotPair.Value);
+			}
+		}
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("[SERVER DEBUG] Rewind Result -> No headshots confirmed."));
 	}
 
-	if (bReturn) return;
+	if (Confirm.BodyShots.Num() > 0)
+	{
+		for (auto& BodyShotPair : Confirm.BodyShots)
+		{
+			if (BodyShotPair.Key)
+			{
+				UE_LOG(LogTemp, Warning, TEXT("[SERVER DEBUG] Rewind Result -> BODYSHOT on %s, Pellet Count: %d"), *BodyShotPair.Key->GetName(), BodyShotPair.Value);
+			}
+		}
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("[SERVER DEBUG] Rewind Result -> No bodyshots confirmed."));
+	}
+
+
+	for (auto& HitCharacter : HitCharacters)
+	{
+		if (HitCharacter == nullptr || Character == nullptr)
+		{
+			UE_LOG(LogTemp, Error, TEXT("[SERVER DEBUG] Damage application skipped for one character due to nullptr."));
+			continue;
+		}
+
+		//데미지 합산 용
+		float TotalDamage = 0.f;
+
+		//HeadShot처리
+		if (Confirm.HeadShots.Contains(HitCharacter))
+		{
+			float HeadShotDamage = Confirm.HeadShots[HitCharacter] * Character->GetEquippedWeapon()->GetDamage();
+			TotalDamage += HeadShotDamage;
+			UE_LOG(LogTemp, Warning, TEXT("[SERVER DEBUG] Calculated Headshot Damage for %s: %.2f"), *HitCharacter->GetName(), HeadShotDamage);
+		}
+
+		//BodyShot처리
+		if (Confirm.BodyShots.Contains(HitCharacter))
+		{
+			float BodyShotDamage = Confirm.BodyShots[HitCharacter] * Character->GetEquippedWeapon()->GetDamage();
+			TotalDamage += BodyShotDamage;
+			UE_LOG(LogTemp, Warning, TEXT("[SERVER DEBUG] Calculated Bodyshot Damage for %s: %.2f"), *HitCharacter->GetName(), BodyShotDamage);
+		}
+
+		if (TotalDamage > 0.f)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("[SERVER DEBUG] Applying Total Damage %.2f to %s"), TotalDamage, *HitCharacter->GetName());
+			//합산한 데미지 적용
+			UGameplayStatics::ApplyDamage(
+				HitCharacter,
+				TotalDamage,
+				Character->Controller,
+				Character->GetEquippedWeapon(),
+				UDamageType::StaticClass()
+			);
+		}
+		else
+		{
+			UE_LOG(LogTemp, Error, TEXT("[SERVER DEBUG] Total damage for %s is 0. Damage not applied."), *HitCharacter->GetName());
+		}
+	}
+	UE_LOG(LogTemp, Warning, TEXT("--- [SERVER DEBUG] ShotgunServerScoreRequest Finished ---\n"));
+}
+//Projectile ServerSideRewind 함수
+FServerSideRewindResult ULagCompensationComponent::ProjectileServerSideRewind(ATPSCharacter* HitCharacter, const FVector_NetQuantize& TraceStart, const FVector_NetQuantize100& InitialVelocity, float HitTime)
+{
+	FFramePackage FrameToCheck = GetFrameToCheck(HitCharacter, HitTime);
+	return ProjectileConfirmHit(FrameToCheck, HitCharacter, TraceStart, InitialVelocity, HitTime);
+}
+FServerSideRewindResult ULagCompensationComponent::ProjectileConfirmHit(const FFramePackage& Package, ATPSCharacter* HitCharacter, const FVector_NetQuantize& TraceStart, const FVector_NetQuantize100& InitialVelocity, float HitTime)
+{
+	//현재 프레임 정보 저장
+	FFramePackage CurrentFrame;
+	CacheBoxPositions(HitCharacter, CurrentFrame);
+	MoveBoxes(HitCharacter, Package);
+	EnableCharacterMeshCollision(HitCharacter, ECollisionEnabled::NoCollision);
+
+	//헤드 먼저 충돌 활성화
+	UBoxComponent* HeadBox = HitCharacter->HitBoxes[FName("head")];
+	HeadBox->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	HeadBox->SetCollisionResponseToChannel(ECC_HitBox, ECollisionResponse::ECR_Block);
+
+	//PredictProjectilePath이용
+	FPredictProjectilePathParams PathParams;
+	PathParams.bTraceWithCollision = true;
+	PathParams.MaxSimTime = MaxRecordTime;
+	PathParams.LaunchVelocity = InitialVelocity;
+	PathParams.StartLocation = TraceStart;
+	PathParams.SimFrequency = 15.f;
+	PathParams.ProjectileRadius = 5.f;
+	PathParams.TraceChannel = ECC_HitBox;
+	PathParams.ActorsToIgnore.Add(GetOwner());
+
+	FPredictProjectilePathResult PathResult;
+	UGameplayStatics::PredictProjectilePath(this, PathParams, PathResult);
+
+	if (PathResult.HitResult.bBlockingHit) //머리에 피격되었다면
+	{
+		ResetHitBoxes(HitCharacter, CurrentFrame);
+		EnableCharacterMeshCollision(HitCharacter, ECollisionEnabled::QueryAndPhysics);
+		return FServerSideRewindResult{ true, true };
+	}
+
+	else //머리에 피격이 아니면 다른 부위 활성화 후 검사
+	{
+		for (auto& HitBoxPair : HitCharacter->HitBoxes)
+		{
+			if (HitBoxPair.Value != nullptr)
+			{
+				HitBoxPair.Value->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+				HitBoxPair.Value->SetCollisionResponseToChannel(ECC_HitBox, ECollisionResponse::ECR_Block);
+			}
+		}
+
+		UGameplayStatics::PredictProjectilePath(this, PathParams, PathResult);
+		if (PathResult.HitResult.bBlockingHit)
+		{
+			ResetHitBoxes(HitCharacter, CurrentFrame);
+			EnableCharacterMeshCollision(HitCharacter, ECollisionEnabled::QueryAndPhysics);
+			return FServerSideRewindResult{ true, false };
+		}
+	}
+
+	ResetHitBoxes(HitCharacter, CurrentFrame);
+	EnableCharacterMeshCollision(HitCharacter, ECollisionEnabled::QueryAndPhysics);
+	return FServerSideRewindResult{ false, false };
 }
 
+void ULagCompensationComponent::ProjectileServerScoreRequest_Implementation(ATPSCharacter* HitCharacter, const FVector_NetQuantize& TraceStart, const FVector_NetQuantize100& InitialVelocity, float HitTime)
+{
+	FServerSideRewindResult Confirm = ProjectileServerSideRewind(HitCharacter, TraceStart, InitialVelocity, HitTime);
 
-
-
+	if (Character && HitCharacter && Confirm.bHitConfirmed)
+	{
+		UGameplayStatics::ApplyDamage(
+			HitCharacter,
+			Character->GetEquippedWeapon()->GetDamage(),
+			Character->Controller,
+			Character->GetEquippedWeapon(),
+			UDamageType::StaticClass()
+		);
+	}
+}
--- TPSMultiPlayGame/Source/TPSMultiPlayGame/TPSComponents/LagCompensationComponent.h
+++ TPSMultiPlayGame/Source/TPSMultiPlayGame/TPSComponents/LagCompensationComponent.h
@@ -32,6 +32,35 @@ struct FFramePackage
 	UPROPERTY()
 	TMap<FName, FBoxInformation> HitBoxInfo;
 
+	UPROPERTY()
+	ATPSCharacter* Character;
+};
+
+//Server-Side Rewind 불리언
+USTRUCT(BlueprintType)
+struct FServerSideRewindResult
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	bool bHitConfirmed;
+
+	UPROPERTY()
+	bool bHeadShot;
+};
+
+//샷건용 Server-Side Rewind 구조체
+USTRUCT(BlueprintType)
+struct FShotgunServerSideRewindResult
+{
+	GENERATED_BODY()
+
+	//피격된 캐릭터들 정보
+	UPROPERTY()
+	TMap<ATPSCharacter*, uint32> HeadShots;
+
+	UPROPERTY()
+	TMap<ATPSCharacter*, uint32> BodyShots;
 };
 
 
@@ -45,13 +74,72 @@ public:
 	friend class ATPSCharacter;
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 	void ShowFramePackage(const FFramePackage& Package, FColor Color);
-	void ServerSideRewind(class ATPSCharacter* HitCharacter, const FVector_NetQuantize& TraceStart, const FVector_NetQuantize& HitLocation, float HitTime);
 
+/// <summary>
+/// HitScan Server-Side Rewind 관련
+/// </summary>
+	UFUNCTION(Server, Reliable)
+	void ServerScoreRequst(ATPSCharacter* HitCharacter, const FVector_NetQuantize& TraceStart,const FVector_NetQuantize& HitLocation,float HitTime);
+	FServerSideRewindResult ServerSideRewind(class ATPSCharacter* HitCharacter, const FVector_NetQuantize& TraceStart, const FVector_NetQuantize& HitLocation, float HitTime);
+	//피격 확인
+	FServerSideRewindResult ConfirmHit(const FFramePackage& Package, ATPSCharacter* HitCharacter,
+		const FVector_NetQuantize& TraceStart, const FVector_NetQuantize& HitLocation);
+
+/// <summary>
+/// 샷건 관련 Server-Side Rewind
+/// </summary>
+	FShotgunServerSideRewindResult ShotgunServerSideRewind(const TArray<ATPSCharacter*>& HitCharacters,
+		const FVector_NetQuantize& TraceStart, const TArray<FVector_NetQuantize>& HitLocations, float HitTime);
+
+	FShotgunServerSideRewindResult ShotgunConfirmHit(const TArray<FFramePackage>& FramePackages,
+		const FVector_NetQuantize& TraceStart, const TArray<FVector_NetQuantize>& HitLocations);
+
+	UFUNCTION(Server, Reliable)
+	void ShotgunServerScoreRequest(const TArray<ATPSCharacter*>& HitCharacters, const FVector_NetQuantize& TraceStart, 
+		const TArray<FVector_NetQuantize>& HitLocations, float HitTime);
+
+/// <summary>
+/// 발사체 Server-Side Rewind
+/// </summary>
+	FServerSideRewindResult ProjectileServerSideRewind(ATPSCharacter* HitCharacter, const FVector_NetQuantize& TraceStart,
+		const FVector_NetQuantize100& InitialVelocity, float HitTime);
+	
+	FServerSideRewindResult ProjectileConfirmHit(const FFramePackage& Package, ATPSCharacter* HitCharacter,
+		const FVector_NetQuantize& TraceStart, const FVector_NetQuantize100& InitialVelocity, float HitTime);
+
+	UFUNCTION(Server, Reliable)
+	void ProjectileServerScoreRequest(
+		ATPSCharacter* HitCharacter,
+		const FVector_NetQuantize& TraceStart,
+		const FVector_NetQuantize100& InitialVelocity,
+		float HitTime
+	);
+	
 protected:
 	virtual void BeginPlay() override;
 	void SaveFramePackage(FFramePackage& Package);
 	FFramePackage InterpBetweenFrams(const FFramePackage& OlderFrame, const FFramePackage& YoungerFrame, float HitTime);
+
 	
+
+	//HitBox 캐시
+	void CacheBoxPositions(ATPSCharacter* HitCharacter, FFramePackage& OutFramePackage);
+	
+	//HitBox이동
+	void MoveBoxes(ATPSCharacter* HitCharacter, const FFramePackage& Package);
+
+	//이동했던 HitBoxes 제자리로
+	void ResetHitBoxes(ATPSCharacter* HitCharacter, const FFramePackage& Package);
+
+	//캐릭터 메시의 충돌 활성화
+	void EnableCharacterMeshCollision(ATPSCharacter* HitCharacter, ECollisionEnabled::Type CollisionEnabled);
+
+	//Tick 에서 호출될 프레임 패키지 저장
+	void SaveFramePackage();
+
+	//피격된 시점의 FramePackage 정보
+	FFramePackage GetFrameToCheck(ATPSCharacter* HitCharacter, float HitTime);
+
 
 private:
 
